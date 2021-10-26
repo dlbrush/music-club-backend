b5eb62f934b237a996fb380ecd0b8a242fcdcae1
Devlin Brush <devlinbrush@DevlinB2021-MAC.local>
2021-10-26 08:59:43 -0500

    Adding validation on SQL definitions and json schemas

--- create-tables.sql
+++ create-tables.sql
@@ -1,31 +1,31 @@
 CREATE TABLE users (
-  username TEXT PRIMARY KEY,
-  password TEXT NOT NULL,
-  email TEXT NOT NULL UNIQUE,
+  username VARCHAR(30) PRIMARY KEY,
+  password VARCHAR(30) NOT NULL,
+  email VA NOT NULL UNIQUE,
   admin BOOLEAN NOT NULL,
   profile_img_url TEXT NOT NULL DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/b/b6/12in-Vinyl-LP-Record-Angle.jpg'
 );
 
 CREATE TABLE clubs (
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
+  name VARCHAR(30) NOT NULL,
   description TEXT NOT NULL,
   founded DATE NOT NULL,
-  founder TEXT REFERENCES users ON DELETE SET NULL,
+  founder VARCHAR(30) REFERENCES users ON DELETE SET NULL,
   is_public BOOLEAN NOT NULL,
   banner_img_url TEXT NOT NULL DEFAULT 'https://images.unsplash.com/photo-1501386761578-eac5c94b800a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
 );
 
 CREATE TABLE users_clubs (
   club_id INTEGER REFERENCES clubs ON DELETE CASCADE,
-  username TEXT REFERENCES users ON DELETE CASCADE,
+  username VARCHAR(30) REFERENCES users ON DELETE CASCADE,
   PRIMARY KEY (club_id, username)
 );
 
 CREATE TABLE invitations (
   club_id INTEGER REFERENCES clubs ON DELETE CASCADE,
-  username TEXT REFERENCES users ON DELETE CASCADE,
-  sent_from TEXT NOT NULL REFERENCES users ON DELETE CASCADE,
+  username VARCHAR(30) REFERENCES users ON DELETE CASCADE,
+  sent_from VARCHAR(30) NOT NULL REFERENCES users ON DELETE CASCADE,
   PRIMARY KEY (club_id, username)
 );
 
@@ -61,7 +61,7 @@ CREATE TABLE albums_genres (
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
-  username TEXT REFERENCES users ON DELETE SET NULL,
+  username VARCHAR(30) REFERENCES users ON DELETE SET NULL,
   comment TEXT NOT NULL,
   post_id INTEGER NOT NULL REFERENCES posts ON DELETE CASCADE,
   posted_at TIMESTAMP NOT NULL
